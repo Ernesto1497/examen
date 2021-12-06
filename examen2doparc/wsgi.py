d62198ce84a5a6c2399b6ccb9f2a64b21a3f3fb0
Ernesto <enrriulv5498@hotmail.com>
2021-12-06 00:43:39 -0600

    archivos adicionales y cambios para Heroku

--- examen2doparc/wsgi.py
+++ examen2doparc/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhithereNoise
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examen2doparc.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
