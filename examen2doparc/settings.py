039a351f77c8f37cbb98bd8e77280a438ec02796
Ernesto <enrriulv5498@hotmail.com>
2021-12-06 14:20:03 -0600

    archivos adicionales y cambios para Heroku

--- examen2doparc/settings.py
+++ examen2doparc/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'blog/static/'
+#STATIC_URL = '../blog/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
