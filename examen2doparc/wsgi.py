2c98a84f2d9007146d29bf5c65f8f09a97a84bfc
Ernesto <enrriulv5498@hotmail.com>
2021-12-06 14:44:02 -0600

    archivos adicionales y cambios para Heroku

--- examen2doparc/wsgi.py
+++ examen2doparc/wsgi.py
@@ -15,5 +15,5 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examen2doparc.settings')
 
-#application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+#application = Cling(get_wsgi_application())
