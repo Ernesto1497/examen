de4e3bda1c4cb1e48b8aff8c1979e5a6a276ee00
Ernesto <enrriulv5498@hotmail.com>
2021-12-06 01:49:35 -0600

    archivos adicionales y cambios para Heroku

--- examen2doparc/settings.py
+++ examen2doparc/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
@@ -125,11 +125,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 5 * 60 #5 minutos estara viva la sesion.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 import dj_database_url
@@ -145,5 +151,3 @@ try:
     from .local_settings import *
 except ImportError:
     pass
-
-
